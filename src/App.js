0a73c73b092418eee64c650c75701f13fc75fbc5
Tasi <tasi87@gmail.com>
2025-08-10 14:09:27 +0200

    Vytvorena navigacia

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 
 // Components
-import MainContent from "./Components/MainContent";
-import Sidebar from "./Components/Sidebar";
+import Navigation from "./Components/Navigation";
 
 function App() {
   return (
 	<div>
 		<header>
-			<nav className="navbar navbar-expand-lg bg-light fixed-top">
-				<section className="container">
-					<div className="navbar-brand">
-						<i className="bi bi-file-earmark-person text-warning fs-3 d-inline-block aligne-middle"></i>
-					</div>
-				</section>
-			</nav>
+			<Navigation />
 		</header>
 	 </div>
   );
---
+++ src/Components/Navigation.js
@@ -0,0 +1,45 @@
+
+
+function Navigation() {
+	return(
+		<nav className="navbar navbar-expand-lg bg-light fixed-top">
+				<section className="container">
+					<div className="navbar-brand">
+						<i className="bi bi-file-earmark-person text-info fs-2 d-inline-block aligne-middle"></i>
+						Životopis
+					</div>
+
+					<button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navMenu">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+
+					<div className="collapse navbar-collapse flex-grow-0" id="navMenu">
+						<ul className="navbar-nav ms-auto">
+							<li className="nav-item">
+								<a href="#AboutMe" className="nav-link">
+									O mne
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="#Education" className="nav-link">
+									Vzdelanie
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="#Experience" className="nav-link">
+									Skúsenosti
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="#Skills" className="nav-link">
+									Zručnosti
+								</a>
+							</li>
+						</ul>
+					</div>
+				</section>
+			</nav>
+	)
+}
+
+export default Navigation;
