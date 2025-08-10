d9968cad57b64a906a363fb8a473dfa53d6e11d4
Tasi <tasi87@gmail.com>
2025-08-10 17:33:19 +0200

    vizualne upravy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from "./Components/Navigation";
 import AboutMe from "./Components/AboutMe";
 import Education from "./Components/Education";
 import Experience from "./Components/Experience";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
 			<AboutMe />
 			<Education />
 			<Experience />
+			<Skills />
 		</main>
 	 </div>
   );
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,21 +1,33 @@
+// image
+import certificate from '../assets/cerifikar_js conv_jpeg/cerifikar_js_1.jpeg';
+
 // styles
-import './Skills';
+import './Skills.css';
 
 const Skills = () => {
 	return (
-		<div>
-			<h2>Zručnosti</h2>
-			<ul>
-				<li>Microsoft Office</li>
-				<li>SAP systém</li>
-				<li>Visual Studio Code</li>
-				<li>HTML a CSS</li>
-				<li>JavaScript</li>
-				<li>TypeScript - ešte sa učím</li>
-				<li>React</li>
-				<li>Bootstrap</li>
-			</ul>
-		</div>
+		<section id="skills" className="container-fluid py-5 skills">
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-lg-6">
+						<h2>Zručnosti</h2>
+						<ul>
+							<li>Microsoft Office</li>
+							<li>SAP systém</li>
+							<li>Visual Studio Code</li>
+							<li>HTML a CSS</li>
+							<li>JavaScript - certifikát</li>
+							<li>TypeScript - ešte sa učím</li>
+							<li>React</li>
+							<li>Bootstrap</li>
+						</ul>
+					</div>
+					<div className="col-12 col-lg-4 text-center align-top">
+						<img className='certificate' src={certificate} alt="Certifikát" />
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
