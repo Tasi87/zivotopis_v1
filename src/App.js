ab4b0ffa512f0c486551a065a1c19e1c74249ad9
Tasi <tasi87@gmail.com>
2025-09-03 11:31:41 +0200

    zmena

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Components
-import Navigation from "./Components/Navigation";
+// import Navigation from "./Components/Navigation";
 import AboutMe from "./Components/AboutMe";
 import Education from "./Components/Education";
 import Experience from "./Components/Experience";
